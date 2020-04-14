e7ece26cf06222d6a9853154c8f4501a180edfd7
kis11 <59667818+kis11@users.noreply.github.com>
2020-04-14 03:57:42 -0500

    new folder

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -11,6 +11,40 @@ from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+def get_email():
+    global emailreceipt,storeproducts2,receiptlist,SENDGRID_API_KEY,MY_EMAIL_ADDRESS,SENDGRID_TEMPLATE_ID,template_data,client,message,response
+    while True:
+        emailreceipt = input("Would you like an email receipt too? Please respond yes or no:")
+        if emailreceipt == "yes" or "Yes" or "YES":
+            storeproducts2 = storeproducts[['id', 'name', 'price']].copy()
+            storeproducts2['price'] = storeproducts2['price'].map('${:,.2f}'.format)
+            storeproducts2 = storeproducts2.take(selectedids) #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.take.html
+            receiptlist = storeproducts2.to_dict('records')
+            load_dotenv()
+            SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+            MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+            SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+            template_data = {
+            "subtotal_price_usd": str('${:.2f}'.format(total_price)),
+            "tax_price_usd": str('${:.2f}'.format(tax)),
+            "total_price_usd": str('${:.2f}'.format(tax + total_price)),
+            "human_friendly_timestamp": date.strftime("%B %d, %Y") + " at " + now.strftime("%I:%M:%S %p"),
+            "products": receiptlist
+            }    
+            client = SendGridAPIClient(SENDGRID_API_KEY) 
+            message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=MY_EMAIL_ADDRESS)
+            message.template_id = SENDGRID_TEMPLATE_ID 
+            message.dynamic_template_data = template_data
+            try:
+                response = client.send(message)
+            except Exception as e:
+                print("Oops, Sendgrid is down. Our bad.", e)
+            break
+        elif emailreceipt == "no" or "No" or "NO": 
+            break
+        else:
+            print("Sorry, try again.")
+
 
 date = datetime.date.today()
 
@@ -60,36 +94,6 @@ print("#> ---------------------------------")
 print("#> THANKS, SEE YOU AGAIN SOON!")
 print("#> ---------------------------------")
 
-while True:
-    emailreceipt = input("Would you like an email receipt too? Please respond yes or no:")
-    if emailreceipt == "yes" or "Yes" or "YES":
-        storeproducts2 = storeproducts[['id', 'name', 'price']].copy()
-        storeproducts2['price'] = storeproducts2['price'].map('${:,.2f}'.format)
-        storeproducts2 = storeproducts2.take(selectedids) #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.take.html
-        receiptlist = storeproducts2.to_dict('records')
-        load_dotenv()
-        SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
-        MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
-        SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
-        template_data = {
-        "subtotal_price_usd": str('${:.2f}'.format(total_price)),
-        "tax_price_usd": str('${:.2f}'.format(tax)),
-        "total_price_usd": str('${:.2f}'.format(tax + total_price)),
-        "human_friendly_timestamp": date.strftime("%B %d, %Y") + " at " + now.strftime("%I:%M:%S %p"),
-        "products": receiptlist
-        }    
-        client = SendGridAPIClient(SENDGRID_API_KEY) 
-        message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=MY_EMAIL_ADDRESS)
-        message.template_id = SENDGRID_TEMPLATE_ID 
-        message.dynamic_template_data = template_data
-        try:
-            response = client.send(message)
-        except Exception as e:
-            print("Oops, Sendgrid is down. Our bad.", e)
-        break
-    elif emailreceipt == "no" or "No" or "NO": 
-        break
-    else:
-        print("Sorry, try again.")
+get_email()
 print("OK, got it. Thanks again.")
 
