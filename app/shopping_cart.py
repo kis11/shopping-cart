e082ad137e422daf0ba81de6bae74eac17aea1cf
kis11 <59667818+kis11@users.noreply.github.com>
2020-04-14 18:43:09 -0500

    fixed email function

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -2,6 +2,8 @@
 
 #from pprint import pprint
 
+#todo: dollar format, tax function
+
 import csv
 import datetime
 import os
@@ -11,43 +13,39 @@ from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def get_email():
-    global emailreceipt,storeproducts2,receiptlist,SENDGRID_API_KEY,MY_EMAIL_ADDRESS,SENDGRID_TEMPLATE_ID,template_data,client,message,response
-    while True:
-        emailreceipt = input("Would you like an email receipt too? Please respond yes or no:")
-        if emailreceipt == "yes" or "Yes" or "YES":
-            storeproducts2 = storeproducts[['id', 'name', 'price']].copy()
-            storeproducts2['price'] = storeproducts2['price'].map('${:,.2f}'.format)
-            storeproducts2 = storeproducts2.take(selectedids) #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.take.html
-            receiptlist = storeproducts2.to_dict('records')
-            load_dotenv()
-            SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
-            MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
-            SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
-            template_data = {
-            "subtotal_price_usd": str('${:.2f}'.format(total_price)),
-            "tax_price_usd": str('${:.2f}'.format(tax)),
-            "total_price_usd": str('${:.2f}'.format(tax + total_price)),
-            "human_friendly_timestamp": date.strftime("%B %d, %Y") + " at " + now.strftime("%I:%M:%S %p"),
-            "products": receiptlist
-            }    
-            client = SendGridAPIClient(SENDGRID_API_KEY) 
-            message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=MY_EMAIL_ADDRESS)
-            message.template_id = SENDGRID_TEMPLATE_ID 
-            message.dynamic_template_data = template_data
-            try:
-                response = client.send(message)
-            except Exception as e:
-                print("Oops, Sendgrid is down. Our bad.", e)
-            break
-        elif emailreceipt == "no" or "No" or "NO": 
-            break
-        else:
-            print("Sorry, try again.")
+
+def calc_tax(number):
+    return number * .0875
+def to_usd(number):
+    return str('${:.2f}'.format(number)) #todo: change the below to to_usd to simplify
+
+def email_info():
+    storeproducts2 = storeproducts[['id', 'name', 'price']].copy()
+    storeproducts2['price'] = storeproducts2['price'].map('${:,.2f}'.format)
+    storeproducts2 = storeproducts2.take(selectedids) #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.take.html
+    receiptlist = storeproducts2.to_dict('records')
+    load_dotenv()
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+    MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+    SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+    template_data = {
+    "subtotal_price_usd": to_usd(total_price),
+    "tax_price_usd": to_usd(tax),
+    "total_price_usd": to_usd(tax + total_price),
+    "human_friendly_timestamp": date.strftime("%B %d, %Y") + " at " + now.strftime("%I:%M:%S %p"),
+    "products": receiptlist
+    }    
+    client = SendGridAPIClient(SENDGRID_API_KEY) 
+    message = Mail(from_email=MY_EMAIL_ADDRESS, to_emails=MY_EMAIL_ADDRESS)
+    message.template_id = SENDGRID_TEMPLATE_ID 
+    message.dynamic_template_data = template_data
+    try:
+        response = client.send(message)
+    except Exception as e:
+        print("Oops, Sendgrid is down. Our bad.", e)
 
 
 date = datetime.date.today()
-
 from datetime import datetime
 now = datetime.now()
 
@@ -55,7 +53,6 @@ csv_filepath = os.path.join(os.path.dirname(__file__), '..', "data", "products.c
 storeproducts = pandas.read_csv(csv_filepath)
 
 total_price = 0
-tax = total_price * .0875
 selectedids = []
 productdict = []
 
@@ -83,17 +80,26 @@ for selectedid in selectedids:
     selectedid = int(selectedid)
     matchingproductprice = storeproducts.loc[selectedid].at["price"]
     matchingproductname = storeproducts.loc[selectedid].at["name"]
-    print("#>" + "  "  +  "..." + "  " + matchingproductname + " " + "(" + str('${:.2f}'.format(matchingproductprice) + ")"))
+    print("#>" + "  "  +  "..." + "  " + matchingproductname + " " + "(" + to_usd(matchingproductprice) + ")")
     total_price = total_price + matchingproductprice 
-tax = total_price * .0875
+tax = calc_tax(total_price)
 print("#> ---------------------------------")
-print("#> SUBTOTAL: " + str('${:.2f}'.format(total_price)))
-print("#> TAX: " + str('${:.2f}'.format(tax)))
-print("#> TOTAL: " + str('${:.2f}'.format(tax + total_price)))
+print("#> SUBTOTAL: " + to_usd(total_price))
+print("#> TAX: " + to_usd(tax))
+print("#> TOTAL: " + to_usd(tax + total_price))
 print("#> ---------------------------------")
 print("#> THANKS, SEE YOU AGAIN SOON!")
 print("#> ---------------------------------")
 
-get_email()
+while True:
+    emailreceipt = input("Would you like an email receipt too? Please respond yes or no:")
+    if emailreceipt in ("yes","Yes","YES"):
+        email_info()
+        break
+    elif emailreceipt in ("no","No","NO"): 
+        break
+    else:
+        print("Sorry, try again.")
+
 print("OK, got it. Thanks again.")
 
